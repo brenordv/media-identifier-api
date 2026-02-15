7c3cad6778b12a4616aab36b9451912ed076f219
Breno RdV <breno@raccoon.ninja>
2026-02-14 23:28:20 -0500

    updated dependency: bumped `simple-log-factory-ext-otel` to 1.3.0 in requirements.txt, refactored `get_otel_log_handler` to use `otel_log_factory` for simplified logger initialization.

--- src/utils.py
+++ src/utils.py
@@ -7,12 +7,12 @@ from simple_log_factory_ext_otel import (
     OtelLogHandler,
     OtelTracer,
     TracedLogger,
-    create_resource,
+    create_resource, otel_log_factory,
 )
 from opentelemetry import trace
 
 request_id_var = contextvars.ContextVar('request_id')
-_otel_logger: TracedLogger | None = None
+
 
 def set_request_id(request_id):
     request_id_var.set(request_id)
@@ -32,37 +32,15 @@ def is_valid_year(year):
     return first_movie_ever_release <= year <= current_year
 
 def get_otel_log_handler(log_name: str) -> TracedLogger:
-    global _otel_logger
-
-    if _otel_logger is not None:
-        return _otel_logger
-
     otel_endpoint = os.environ.get("OTEL_EXPORTER_OTLP_ENDPOINT")
 
     if not otel_endpoint:
         raise ValueError("OTEL_EXPORTER_OTLP_ENDPOINT environment variable must be set.")
 
     service_name = "media-identifier-api"
-    resource = create_resource(service_name)
 
-    log_handler = OtelLogHandler(
+    return otel_log_factory(
         service_name=service_name,
-        endpoint=f"{otel_endpoint}/v1/logs",
-        protocol="http",
-        resource=resource,
+        log_name=log_name,
+        otel_exporter_endpoint=otel_endpoint
     )
-
-    otel_tracer = OtelTracer(
-        service_name=service_name,
-        endpoint=f"{otel_endpoint}/v1/traces",
-        protocol="http",
-        resource=resource,
-    )
-
-    trace.set_tracer_provider(otel_tracer.provider)
-
-    logger = log_factory(log_name=log_name, custom_handlers=[log_handler])
-
-    _otel_logger = TracedLogger(logger=logger, tracer=otel_tracer.tracer)
-
-    return _otel_logger
